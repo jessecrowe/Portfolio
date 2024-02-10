5c410cfa6bd49f61ee9dda874280854e2e44fca1
Jesse Crowe <jesse.crowe@mykenzie.snhu.edu>
2024-02-10 16:27:33 -0700

    portfolio in progress

---
+++ code.js
@@ -0,0 +1,6 @@
+function toggleBurger() {
+    const menu = document.querySelector(".burger-links");
+    const burgerImage = document.querySelector(".burger-image");
+    menu.classList.toggle("open")
+    burgerImage.classList.toggle("open")
+}
